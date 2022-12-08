72d72869d1882e1366c3dec0b54feda4bacba24f
nonso01 <chukwunonsomartin01@gmail.com>
2022-12-08 07:57:02 -0800

    c lang

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - just a string 
+*Return: a string
+*/
+int main(void)
+{
+char art[] = "with proper grammer, but the outcome is a piece of art,";
+printf("%c\n",art);
+return (0);
+}
