cb49ecb2bdd97cdc023d33952394014e6a90e26d
nonso01 <chukwunonsomartin01@gmail.com>
2022-12-08 07:37:23 -0800

    c lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-char puzzle[]= "\"Programming is like building a multilingual puzzle";
-puts (puzzle);
+char puzzle[] = "\"Programming is like building a multilingual puzzle";
+puts(puzzle);
 return (0);
 }
