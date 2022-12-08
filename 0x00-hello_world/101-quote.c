0bac52c283a5dfce8ca9e7d02df7a810818e8b02
nonso01 <chukwunonsomartin01@gmail.com>
2022-12-08 11:47:23 -0800

    shell

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - making use of a different function
+*Return: 0
+*/
+int main(void)
+{
+char word[] = "and  that piece of art is useful\" - Dora Korpar, 2015-10-1";
+puts(word);
+return (1);
+}
