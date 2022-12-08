3fd7c079533e200e85747783533ac486c3bb3cc8
nonso01 <chukwunonsomartin01@gmail.com>
2022-12-08 11:48:42 -0800

    shell

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-char word[] = "and  that piece of art is useful\" - Dora Korpar, 2015-10-1";
+char word[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-1";
 puts(word);
 return (1);
 }
