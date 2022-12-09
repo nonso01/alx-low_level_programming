b6733bfa6c8bd0e4532804c9e987b8270b0ae50f
nonso01 <chukwunonsomartin01@gmail.com>
2022-12-09 12:22:34 -0800

    c lang

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - prints out alphabets
+*Return: 0
+*/
+
+int main(void)
+{
+char chr;
+for (chr = 'a'; chr <= 'z'; ++chr)
+printf("%c ",  chr);
+return (0);
+}
